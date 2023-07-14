6ab676f5225bbd2632443922b6d90f560551ab25
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-14 14:29:04 +0700

    v1.2 Defined routes using 'app.use()'

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ require("dotenv").config({
   path: path.resolve(__dirname, '../.env')
 })
 const port = process.env.PORT || 8000;
-const express = require("express");
 const db = require("./models");
+
+const express = require("express");
 const app = express();
 
 const { branchRoutes } = require("./routes");
+// const { home } = require("./");
 
 app.use(express.json());
 
 app.use("/branch-management", branchRoutes);
-// app.use("/branch-management", branchRoutes);
+app.use("/api/auth", branchRoutes);                         // [POST] Register Account
+app.use("/api/auth/verify", branchRoutes);                  // [PATCH] Verify Account
+app.use("/api/auth/login", branchRoutes);                   // [POST] Login
+                                                            // [GET] Keep Login
+app.use("/api/auth/forgotPass", branchRoutes);              // [PUT] Forgot Password
+app.use("/api/auth/resetPass", branchRoutes);
+app.use("/api/auth/changeUsername", branchRoutes);
+app.use("/api/auth/changeEmail", branchRoutes);
+app.use("/api/profile/single-uploaded", branchRoutes);
+app.use("/api/blog/remove/:id", branchRoutes);
+app.use("/api/blog?id_cat=3&sort=ASC&page898", branchRoutes);
+app.use("/api/blog/allCategory", branchRoutes);
+app.use("/api/blog/log", branchRoutes);
+app.use("/api/blog/like", branchRoutes);
+app.use("/api/blog/pagLike", branchRoutes);
+app.use("/api/blog/pagFav", branchRoutes);
+app.use("/api/blog/pagFav", branchRoutes);
+app.use("/Public/Blog-1683581987195226008.png", branchRoutes);
+app.use("/Public/Avatar-6.png", branchRoutes);
