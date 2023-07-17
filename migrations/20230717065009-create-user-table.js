32dd9f7a49eeaac58f4d4be03712e197f5490a72
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-17 15:20:30 +0700

    v4.1.1.2 Added Query Interface on 'up' method

--- migrations/20230717065009-create-user-table.js
+++ migrations/20230717065009-create-user-table.js
@@ -9,6 +9,37 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('users', {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.NUMBER
+      },
+      avatar: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      isVerified: {
+        type: Sequelize.BOOLEAN
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
