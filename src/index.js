010cea556e728571bf891275c6a06195c72195d5
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-17 12:02:02 +0700

    Integrated Index Routes into index.js (outermost) with 'authRoutes' variable

--- src/index.js
+++ src/index.js
@@ -8,30 +8,30 @@ const db = require("./models");
 const express = require("express");
 const app = express();
 
-const { branchRoutes } = require("./routes");
+const { authRoutes } = require("./routes");
 // const { home } = require("./");
 
 app.use(express.json());
 
-app.use("/branch-management", branchRoutes);
-app.use("/api/auth", branchRoutes);                         // [POST] Register Account
-app.use("/api/auth/verify", branchRoutes);                  // [PATCH] Verify Account
-app.use("/api/auth/login", branchRoutes);                   // [POST] Login
+app.use("/branch-management", authRoutes);
+app.use("/api/auth", authRoutes);                         // [POST] Register Account
+app.use("/api/auth/verify", authRoutes);                  // [PATCH] Verify Account
+app.use("/api/auth/login", authRoutes);                   // [POST] Login
                                                             // [GET] Keep Login
-app.use("/api/auth/forgotPass", branchRoutes);              // [PUT] Forgot Password
-app.use("/api/auth/resetPass", branchRoutes);
-app.use("/api/auth/changeUsername", branchRoutes);
-app.use("/api/auth/changeEmail", branchRoutes);
-app.use("/api/profile/single-uploaded", branchRoutes);
-app.use("/api/blog/remove/:id", branchRoutes);
-app.use("/api/blog?id_cat=3&sort=ASC&page898", branchRoutes);
-app.use("/api/blog/allCategory", branchRoutes);
-app.use("/api/blog/log", branchRoutes);
-app.use("/api/blog/like", branchRoutes);
-app.use("/api/blog/pagLike", branchRoutes);
-app.use("/api/blog/pagFav", branchRoutes);
-app.use("/api/blog/pagFav", branchRoutes);
-app.use("/Public/Blog-1683581987195226008.png", branchRoutes);
-app.use("/Public/Avatar-6.png", branchRoutes);
+app.use("/api/auth/forgotPass", authRoutes);              // [PUT] Forgot Password
+app.use("/api/auth/resetPass", authRoutes);
+app.use("/api/auth/changeUsername", authRoutes);
+app.use("/api/auth/changeEmail", authRoutes);
+app.use("/api/profile/single-uploaded", authRoutes);
+app.use("/api/blog/remove/:id", authRoutes);
+app.use("/api/blog?id_cat=3&sort=ASC&page898", authRoutes);
+app.use("/api/blog/allCategory", authRoutes);
+app.use("/api/blog/log", authRoutes);
+app.use("/api/blog/like", authRoutes);
+app.use("/api/blog/pagLike", authRoutes);
+app.use("/api/blog/pagFav", authRoutes);
+app.use("/api/blog/pagFav", authRoutes);
+app.use("/Public/Blog-1683581987195226008.png", authRoutes);
+app.use("/Public/Avatar-6.png", authRoutes);
 
 //this comment added for requirement of git changes tracker file
