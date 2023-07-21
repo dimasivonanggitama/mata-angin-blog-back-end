dbfbb6e803addc68d2d0362a505f8c1b3e9bc59d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-21 10:30:51 +0700

    v5.1.2.2 Added 'return User;'

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("User", {
+        user_id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        username: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+        email: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+        password: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+        phone: {
+            allowNull: false,
+            type: Sequelize.BIGINT
+        },
+        avatar: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+        isVerified: {
+            type: Sequelize.BOOLEAN
+        },
+        updatedAt: false,
+        timestamps: false,
+    });
+    return User;
+}
