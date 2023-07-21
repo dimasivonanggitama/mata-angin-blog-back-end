e325fae53467033da0e183383dba21354bf9bd88
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-21 14:12:45 +0700

    v1.7.3.3 Commented unused defined routes

--- src/index.js
+++ src/index.js
@@ -13,25 +13,25 @@ const { authRoutes } = require("./routes");
 
 app.use(express.json());
 
-app.use("/branch-management", authRoutes);
+// app.use("/branch-management", authRoutes);
 app.use("/api/auth", authRoutes);                         // [POST] Register Account
-app.use("/api/auth/verify", authRoutes);                  // [PATCH] Verify Account
+// app.use("/api/auth/verify", authRoutes);                  // [PATCH] Verify Account
 app.use("/api/auth/login", authRoutes);                   // [POST] Login
                                                             // [GET] Keep Login
-app.use("/api/auth/forgotPass", authRoutes);              // [PUT] Forgot Password
-app.use("/api/auth/resetPass", authRoutes);
-app.use("/api/auth/changeUsername", authRoutes);
-app.use("/api/auth/changeEmail", authRoutes);
-app.use("/api/profile/single-uploaded", authRoutes);
-app.use("/api/blog/remove/:id", authRoutes);
-app.use("/api/blog?id_cat=3&sort=ASC&page898", authRoutes);
-app.use("/api/blog/allCategory", authRoutes);
-app.use("/api/blog/log", authRoutes);
-app.use("/api/blog/like", authRoutes);
-app.use("/api/blog/pagLike", authRoutes);
-app.use("/api/blog/pagFav", authRoutes);
-app.use("/api/blog/pagFav", authRoutes);
-app.use("/Public/Blog-1683581987195226008.png", authRoutes);
-app.use("/Public/Avatar-6.png", authRoutes);
+// app.use("/api/auth/forgotPass", authRoutes);              // [PUT] Forgot Password
+// app.use("/api/auth/resetPass", authRoutes);
+// app.use("/api/auth/changeUsername", authRoutes);
+// app.use("/api/auth/changeEmail", authRoutes);
+// app.use("/api/profile/single-uploaded", authRoutes);
+// app.use("/api/blog/remove/:id", authRoutes);
+// app.use("/api/blog?id_cat=3&sort=ASC&page898", authRoutes);
+// app.use("/api/blog/allCategory", authRoutes);
+// app.use("/api/blog/log", authRoutes);
+// app.use("/api/blog/like", authRoutes);
+// app.use("/api/blog/pagLike", authRoutes);
+// app.use("/api/blog/pagFav", authRoutes);
+// app.use("/api/blog/pagFav", authRoutes);
+// app.use("/Public/Blog-1683581987195226008.png", authRoutes);
+// app.use("/Public/Avatar-6.png", authRoutes);
 
 //this comment added for requirement of git changes tracker file
