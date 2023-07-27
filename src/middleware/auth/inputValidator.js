e190db4791a3fc825fb1328f0dd158a435ecf9d5
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-28 06:34:34 +0700

    v6.1.2.5 Added 'emailValidator' function

--- src/middleware/auth/inputValidator.js
+++ src/middleware/auth/inputValidator.js
@@ -1,22 +1,50 @@
 const emailValidator = (req, res, next) => {
-    // const { username, email, password, phone } = req.body;
-    // if (email.isEmpty()) return next();
-    // else return next();
+    let result = false;
+    let { email } = req.body;
+    if (email == false) return res.status(422).send("Email tidak boleh kosong!");
 
-    // return 
+    email = email.toLowerCase();
+
+    // dimasivonanggitama @ gmail    .com
+    // ------------------ | ------   ----------------
+    //     email name     | domain   top level domain
+    //                    |
+    //              at / separator
+
+    const atChar = email.indexOf("@");
+    if (atChar < 0) return res.status(422).send("Email harus memiliki karakter '@' !");
+
+    const domain = email.substr(atChar + 1, email.length - 1);
+    const dotCharDomain = domain.indexOf(".");
+    if (dotCharDomain < 0) return res.status(422).send("Domain email harus memiliki karakter titik (.) !");
+
+    const domainPattern = /[a-z.]/;
+    for (let i = 0; i < domain.length; i++) {
+        result = domainPattern.test(domain[i]);
+        if (result == false) return res.status(422).send("Karakter pada domain yang diperbolehkan hanya huruf dan titik (.) !");
+    }
+
+    const aliasName = email.substr(0, atChar);
+    const aliasNamePattern = /[a-z0-9._]/;
+    for (let i = 0; i < aliasName.length; i++) {
+        result = aliasNamePattern.test(aliasName[i]);
+        if (result == false) return res.status(422).send("Karakter pada nama email yang diperbolehkan hanya huruf, angka, titik (.) dan underscore (_) !");
+    }
+
     return next();
 };
 
 const usernameValidator = (req, res, next) => {
-    const { username } = req.body;
-    if (username.isEmpty()) return res.status(422).send("Username tidak boleh kosong!");
+    let result = false;
+    let { username } = req.body;
+    if (username == false) return res.status(422).send("Username tidak boleh kosong!");
+    // if (username == false) res.status(422).send("Username tidak boleh kosong!");
 
     username = username.toLowerCase();
-    let result = false;
     const pattern = /[a-z0-9._]/;
-    for (let i = 0; i < text.length; i++) {
-        result = pattern.test(text[i]);
-        if (result == false) return res.status(422).send("Karakter yang diperbolehkan hanya huruf, angka, titik (.) dan underscore (_)!");
+    for (let i = 0; i < username.length; i++) {
+        result = pattern.test(username[i]);
+        if (result == false) return res.status(422).send("Karakter yang diperbolehkan hanya huruf, angka, titik (.) dan underscore (_) !");
     }
     return next();
 }
@@ -29,4 +57,4 @@ const phoneValidator = (req, res, next) => {
     return next();
 }
 
-module.exports = { inputVerificator };
+module.exports = { usernameValidator, emailValidator };
