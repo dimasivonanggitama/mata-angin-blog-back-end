7d142688ca682835001fc4d2b60646adf4dae885
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-28 21:07:08 +0700

    v2.1.2.6 Added 'phoneValidator' middleware on Register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
 const { authController } = require("../controllers");
 const router = require("express").Router();
-const { usernameValidator, emailValidator, passwordValidator } = require("../middleware");
+const { usernameValidator, emailValidator, passwordValidator, phoneValidator } = require("../middleware");
 
 router.post("/login", authController.login);
-router.post("/register", usernameValidator, emailValidator, passwordValidator, authController.register);
+router.post("/register", usernameValidator, emailValidator, passwordValidator, phoneValidator, authController.register);
 
 module.exports = router;
