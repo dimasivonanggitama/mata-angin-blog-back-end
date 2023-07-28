51296638e3081a01f51dfecdbc0045a03bcf3940
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-28 21:06:40 +0700

    v2.1.2.5 Added 'passwordValidator' middleware on Register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
 const { authController } = require("../controllers");
 const router = require("express").Router();
-const { usernameValidator, emailValidator } = require("../middleware");
+const { usernameValidator, emailValidator, passwordValidator } = require("../middleware");
 
 router.post("/login", authController.login);
-router.post("/register", usernameValidator, emailValidator, authController.register);
+router.post("/register", usernameValidator, emailValidator, passwordValidator, authController.register);
 
 module.exports = router;
