253bd7b51855ac88c174bea6f92ce236ee6b788d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-30 20:44:25 +0700

    v2.1.2.7 Added 'dbVerificator' middleware on Register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
 const { authController } = require("../controllers");
 const router = require("express").Router();
-const { usernameValidator, emailValidator, passwordValidator, phoneValidator } = require("../middleware");
+const { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator } = require("../middleware");
 
 router.post("/login", authController.login);
-router.post("/register", usernameValidator, emailValidator, passwordValidator, phoneValidator, authController.register);
+router.post("/register", usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, authController.register);
 
 module.exports = router;
