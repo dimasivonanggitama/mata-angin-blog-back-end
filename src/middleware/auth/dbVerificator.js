d27a2ab5fe2d989190e4e4acac98bab7783885be
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-30 14:50:14 +0700

    v6.1.3.3 Added dbVerificator() function

--- src/middleware/auth/dbVerificator.js
+++ src/middleware/auth/dbVerificator.js
@@ -1,1 +1,31 @@
-module.exports = {}
+const db = require('../models');
+const users = db.user;
+
+const dbVerificator = async (req, res, next) => {
+    const { email, username, phone } = req.body;
+    try {
+        const emailExist = await users.findOne({
+            where: { username: email },
+        });
+        if (emailExist) return res.status(409).json({ message: "Email sudah digunakan sebelumnya" });
+
+        const usernameExist = await users.findOne({
+            where: { username: username },
+        });
+        if (usernameExist) return res.status(409).json({ message: "Username sudah digunakan sebelumnya" });
+        
+        const phoneExist = await users.findOne({
+            where: { username: phone },
+        });
+        if (phoneExist) return res.status(409).json({ message: "Nomor telepon sudah digunakan sebelumnya" });
+
+        next();
+    } catch (err) {
+        return res.status(503).json({
+            message: 'Mohon maaf, layanan tidak tersedia saat ini. Silakan coba lagi nanti.',
+            error: err.message
+        });
+    }
+}
+
+module.exports = { dbVerificator }
