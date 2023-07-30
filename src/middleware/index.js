e372637671a03e214888a9b6b32faf7ff7333675
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-30 20:51:08 +0700

    v6.1.1.6 Added 'dbVerificator' reference from Auth middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,3 +1,4 @@
 const { usernameValidator, emailValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
+const { dbVerificator } = require("./auth/dbVerificator");
 
-module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator };
+module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator };
