3e41030a2ce6de89704da5fe1bcbfffcf8a8493a
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-03 15:44:03 +0700

    added multer to index middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
 const { usernameValidator, emailValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
 const { dbVerificator } = require("./auth/dbVerificator");
 const { tokenVerificator } = require("./auth/tokenVerificator");
+const { multerUpload } = require("./blog/multer");
 
-module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator };
+module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator, multerUpload };
