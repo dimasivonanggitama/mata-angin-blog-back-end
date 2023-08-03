4e237522cdb3e1575a1dcc422aefd5aca87d21a2
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-03 11:05:24 +0700

    Added tokenVerificator function

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const { usernameValidator, emailValidator, passwordValidator, phoneValidator } = require("./auth/inputValidator");
 const { dbVerificator } = require("./auth/dbVerificator");
+const { tokenVerificator } = require("./auth/tokenVerificator");
 
-module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator };
+module.exports = { usernameValidator, emailValidator, passwordValidator, phoneValidator, dbVerificator, tokenVerificator };
