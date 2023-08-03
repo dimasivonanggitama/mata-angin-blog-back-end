adf7a4b733585be9c71906cfaa809f48ee706230
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-03 17:04:31 +0700

    change format file name to 'image'

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -7,5 +7,5 @@ const { tokenVerificator, multerUpload } = require("../middleware");
 // router.post("/likes", authController.login);
 // router.post("/posts", authController.login);
 // router.post("/remove", authController.login);
-router.post("/create", tokenVerificator, multerUpload.single("blog"), blogController.createBlog);
+router.post("/create", tokenVerificator, multerUpload.single("image"), blogController.createBlog);
 module.exports = router;
