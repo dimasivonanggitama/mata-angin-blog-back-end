42bb929b8bb71675bfa5439104832360ea130028
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-03 11:28:19 +0700

    Added blog.js route

---
+++ src/routes/blog.js
@@ -0,0 +1,12 @@
+const { blogController } = require("../controllers");
+const router = require("express").Router();
+const { tokenVerificator } = require("../middleware");
+
+// router.post("/categories", authController.login);
+// router.post("/favorites", authController.login);
+// router.post("/likes", authController.login);
+// router.post("/posts", authController.login);
+// router.post("/remove", authController.login);
+router.post("/create", tokenVerificator, blogController.createBlog);
+
+module.exports = router;
